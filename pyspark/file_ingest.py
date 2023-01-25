ae7141a9e7b2ea33283b7ecc6c71337918aae6eb
Sean Coyne <scoyne@goodrx.com>
2023-01-25 15:07:26 -0800

    Update file_ingest.py

--- pyspark/file_ingest.py
+++ pyspark/file_ingest.py
@@ -1,4 +1,5 @@
 import argparse
+from datetime import datetime
 import logging
 import re
 
@@ -36,25 +37,33 @@ if __name__ == "__main__":
     # check if header has '\t' if so set delimiter
     rdd = spark.textFile(input_file).take(1)
     header = rdd.first()
-    result = re.search("(,|;|\\|)", header)
-    delimiter = result.group()
+    if header.count('|') > 1:
+        delimiter = '|'
+    elif header.count('|') > 1:
+        delimiter = ','
+    # check if header has at least 2 " if so set quotes on
+    isQuoted = header.count('"') > 2
     
-    
+    # check if file is compressed
+    compression = None
+    if 'gzip' in file_extension:
+        compression = 'gzip    
 
+    # Get a count of the raw file rows, log it and store as variable
 
-# check if header has at least 2 " if so set quotes on
+    # read the input file, header is required
+    df = spark.read.csv(input_file, sep=delimiter, header=True, quoteAll=isQuoted, inferSchema=True, compression=compression)
 
-# Get a count of the raw file rows, log it and store as variable
+    # write to parquet, allow overwrite. partitioned by /FILENAME/dt=YYYY-MM-DD/
+    today = datetime.today().strftime('%Y-%m-%d')
+    output_path = f"{output_path}/{}/dt={today}/"
+    df.write.parquet(path=output_path, mode='overwrite')
 
-# read the input file
+    # Log output file path
 
-# write to parquet, allow overwrite. partitioned by /FILENAME/dt=YYYY-MM-DD/
+    # Get a count of the output file rows, log it along with the input count
 
-# Log output file path
+    # mock out as comments: Run great expectations check (how to get results data into postgres?)
 
-# Get a count of the output file rows, log it along with the input count
-
-# mock out as comments: Run great expectations check (how to get results data into postgres?)
-
-# mock out as comments: make call to API to move on to next steps
-#  (update posgres table, trigger glue crawlers, etc)
+    # mock out as comments: make call to API to move on to next steps
+    #  (update posgres table, trigger glue crawlers, etc)
