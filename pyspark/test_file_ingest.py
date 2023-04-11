0c2090db66d5047806b7f60bbbe8e3d815edc33a
Sean Coyne <scoyne@goodrx.com>
2023-04-10 22:26:56 -0700

    Update test_file_ingest.py

--- pyspark/test_file_ingest.py
+++ pyspark/test_file_ingest.py
@@ -12,12 +12,26 @@ from file_ingest import (
 
 
 class TestMyScript(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.spark = (
-            SparkSession.builder.appName("test").enableHiveSupport().getOrCreate()
-        )
+    def setUp(self):
+        self.spark = SparkSession.builder.appName("TestInspectFile").getOrCreate()
 
+    def tearDown(self):
+        self.spark.stop()
+        
     def test_create_spark_session(self):
         spark = create_spark_session("test")
         self.assertIsInstance(spark, SparkSession)
+        
+    def test_inspect_file(self):
+        header1 = 'Name, Age, Gender'
+        header2 = 'Name|Age|Gender'
+        header3 = '"Name","Age","Gender"'
+        delimiter1, quote1 = inspect_file(self.spark, header1)
+        delimiter2, quote2 = inspect_file(self.spark, header2)
+        delimiter3, quote3 = inspect_file(self.spark, header3)
+        self.assertEqual(delimiter1, ",")
+        self.assertEqual(delimiter2, "|")
+        self.assertEqual(delimiter3, ",")
+        self.assertEqual(quote1, "")
+        self.assertEqual(quote2, "")
+        self.assertEqual(quote3, '"')
