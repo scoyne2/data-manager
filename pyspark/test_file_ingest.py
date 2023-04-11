544ef5fda41ea27ed564af908288638de3c5ff87
Sean Coyne <scoyne@goodrx.com>
2023-04-10 22:32:20 -0700

    Update test_file_ingest.py

--- pyspark/test_file_ingest.py
+++ pyspark/test_file_ingest.py
@@ -1,19 +1,22 @@
 import unittest
+import boto3
+import yaml
 from unittest.mock import patch
 
 from pyspark.sql.session import SparkSession
 
 from file_ingest import (
     create_spark_session,
-    # inspect_file,
-    # process_file,
-    # perform_quality_checks,
+    inspect_file,
+    process_file,
+    perform_quality_checks,
 )
 
 
 class TestMyScript(unittest.TestCase):
     def setUp(self):
         self.spark = SparkSession.builder.appName("TestInspectFile").getOrCreate()
+        self.output_path = "path/to/output/folder"
 
     def tearDown(self):
         self.spark.stop()
@@ -35,3 +38,44 @@ class TestMyScript(unittest.TestCase):
         self.assertEqual(quote1, "")
         self.assertEqual(quote2, "")
         self.assertEqual(quote3, '"')
+        
+    def test_process_file(self):
+        input_df = self.spark.createDataFrame([
+            ("John", 25, "Male"),
+            ("Mary", 30, "Female")
+        ], ["Name", "Age", "Gender"])
+
+        process_file(self.spark, input_df, self.output_path)
+        today = datetime.today().strftime("%Y-%m-%d")
+        output_file = f"{self.output_path}/dt={today}/"
+        self.assertTrue(os.path.isdir(output_file))
+        self.assertEqual(len(os.listdir(output_file)), 1)
+        self.assertEqual(os.listdir(output_file)[0], "_SUCCESS")
+        output_count = self.spark.read.parquet(output_file).count()
+        self.assertEqual(output_count, 2)
+
+    @patch('boto3.Session.client')
+    def test_perform_quality_checks(self, mock_client):
+        output_path = 'path/to/output'
+        resources_bucket = 'my-bucket'
+        # Prepare mock response
+        expected_config = {
+            'datasources': {
+                'mydatasource': {
+                    'data_asset_type': 'SparkDFDataset',
+                    'data_asset_options': {
+                        'path': output_path,
+                        'file_format': 'parquet'
+                    }
+                }
+            }
+        }
+        mock_body = yaml.safe_dump(expected_config)
+        mock_response = {'Body': mock_body}
+        mock_client.return_value.get_object.return_value = mock_response
+
+        # Call the function
+        perform_quality_checks(output_path, resources_bucket)
+
+        # Check if mock client was called with the correct parameters
+        mock_client.assert_called_with('s3')
